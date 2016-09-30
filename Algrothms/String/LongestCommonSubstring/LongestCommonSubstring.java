1c176dc5a1b9fb9844627f7e65015a70fa2203e7
Tom Lazar <thomas.lazar@marquette.edu>
2016-09-30 17:43:37 -0500

    Changed Code Style
    Changed the code style to match, Vanilla Java

--- Algrothms/String/LongestCommonSubstring/LongestCommonSubstring.java
+++ Algrothms/String/LongestCommonSubstring/LongestCommonSubstring.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 /**
  * Created by tom lazar on 9/26/2016.
  */
-public class LongestCommonSubstring
-{
+public class LongestCommonSubstring {
 
     // Fields
     final static boolean DEBUG = true;
@@ -15,32 +14,54 @@ public class LongestCommonSubstring
     private int MisMatchPenalty;
     private int InsertionPenalty;
     private int DeletionPenalty;
+    private boolean GlobalAllignment;
 
     //Constructors
 
-
-    public LongestCommonSubstring()
-    {
-      MatchBonus = 2;
-      MisMatchPenalty = -1;
-      InsertionPenalty = -1;
-      DeletionPenalty = -1;
+    /**
+     * Creates a LCS object with default score values, this can be used
+     * to find different types of string alligments including.
+     * - Global alligment
+     * - Semi-Global alligment
+     * - Local alligment
+     *
+     * @return default object.
+     */
+    public LongestCommonSubstring() {
+        MatchBonus = 2;
+        MisMatchPenalty = -1;
+        InsertionPenalty = -1;
+        DeletionPenalty = -1;
+        GlobalAllignment = true;
     }
 
+    /**
+     * Creates a LCS object with non-default Parameters
+     *
+     * @return Object
+     * @matchBonus Score for a match between two chars
+     * @misMatchPenalty Penalty for a mismatch between two chars
+     * @insertionPenalty Penalty for inserting a char into the sequence
+     * @deletionPenalty Penalty for deleteing a char from the sequence
+     * @globalAllignment If the value is true, then the worst score possible is zero
+     */
     public LongestCommonSubstring(int matchBonus, int misMatchPenalty,
-                                  int insertionPenalty, int deletionPenalty)
-    {
-      MatchBonus = matchBonus;
-      MisMatchPenalty = misMatchPenalty;
-      InsertionPenalty = insertionPenalty;
-      DeletionPenalty = deletionPenalty;
+                                  int insertionPenalty, int deletionPenalty,
+                                  boolean globalAllignment) {
+        MatchBonus = matchBonus;
+        MisMatchPenalty = misMatchPenalty;
+        InsertionPenalty = insertionPenalty;
+        DeletionPenalty = deletionPenalty;
+        GlobalAllignment = globalAllignment;
     }
 
-    public static void InteractiveRunner(String[] args)
-    {
+    /**
+     * Runs the program all interactive like.
+     */
+    public static void InteractiveRunner() {
         char cont; //user input for weather or not the program will continue through multiple runs.
-        do
-        {
+        LongestCommonSubstring lcs = new LongestCommonSubstring();
+        do {
             // Vars for input strings
             String S, T;
 
@@ -52,14 +73,13 @@ public class LongestCommonSubstring
             System.out.print("Enter the second sequence: ");
             T = s.nextLine();
 
-            if (DEBUG)
-            {
+            if (DEBUG) {
                 System.out.println("FIRST SEQUENCE: " + S);
                 System.out.println("SECOND SEQUENCE: " + T);
             }
 
             // ** PROCESS TABLE **
-            String res = lcs(S, T);
+            String res = lcs.lcs(S, T);
             System.out.println(res);
 
             // Decide next Program
@@ -70,35 +90,45 @@ public class LongestCommonSubstring
         System.out.println("Bye!");
     }
 
-    /** Returns the Largest common substring for this sequence
+    /**
+     * Returns the Largest common substring for this sequence
+     *
      * @param S the first input string
      * @param T the second input string
      * @return the longest common subsequence of these two.
      */
-    public static String lcs(String S, String T)
-    {
+    public String lcs(String S, String T) {
         int[][] table = new int[S.length() + 1][T.length() + 1];
 
         // row 0 and column 0 are initialized to 0 already
         for (int i = 0; i < S.length(); i++)
-            for (int j = 0; j < T.length(); j++)
+            for (int j = 0; j < T.length(); j++) {
+                char a = S.charAt(i),
+                        b = T.charAt(j);
+
                 if (S.charAt(i) == T.charAt(j)) // MATCHING
-                    table[i + 1][j + 1] = table[i][j] + 2;
-                else
-                    table[i + 1][j + 1] = // INSERTION/DELETION
-                            Math.max(Math.max(table[i + 1][j] - 1, table[i][j + 1] - 1), 0);
+                    table[i + 1][j + 1] = table[i][j] + MatchBonus;
+                else if (a != b)                 // mismatch
+                    table[i + 1][j + 1] = table[i][j] + MisMatchPenalty;
+                else {                           // INSERTION/DELETION
+                    int score = Math.max(table[i + 1][j] - InsertionPenalty
+                            , table[i][j + 1] - DeletionPenalty);
+                    if (GlobalAllignment) {
+                        score = Math.max(score, 0);
+                    }
+                    table[i + 1][j + 1] = score;
+                }
+            }
 
         // read the substring out from the matrix
         StringBuffer out = new StringBuffer();
         for (int x = S.length(), y = T.length(), score = 0;
-             x != 0 && y != 0; )
-        {
+             x != 0 && y != 0; ) {
             if (table[x][y] == table[x - 1][y])
                 x--;
             else if (table[x][y] == table[x][y - 1])
                 y--;
-            else
-            {
+            else {
                 assert S.charAt(x - 1) == T.charAt(y - 1);
                 out.append(S.charAt(x - 1));
                 x--;
